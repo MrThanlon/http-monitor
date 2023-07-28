b16b7e73fae36594d8cd860d10ec77fbbf090ca5
黄子懿 <huangziyi@canaan-creative.com>
2023-07-28 17:36:23 +0800

    update
    Signed-off-by: 黄子懿 <huangziyi@canaan-creative.com>

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ if __name__ == '__main__':
         time.sleep(60 * 5)
         for idx, c in enumerate(config.process):
             if not check_alive(c['url']):
+                print(f"{c['command'][0]} down, restarting")
                 process[idx].terminate()
                 process[idx].wait()
                 process[idx] = subprocess.Popen(c['command'], cwd=c['cwd'])
+                print(f"{c['command'][0]} restarted")
+            else:
+                print(f"{c['command']} is alive")
